e5c1c10cfc3a4a700dfe4e8ad4abcee1f64fbf21
K1NGAARON <gerardaaron1997@gmail.com>
2023-03-21 09:26:48 +0100

    changes to CSS

--- js/app.js
+++ js/app.js
@@ -2,17 +2,29 @@ function yellowBar(e) {
     const header = document.querySelector('.header .header-menu__menu ul');
     const footer = document.querySelector('.footer-menu__menu ul');
 
-    const elementToAdd = `
+    const headerBar = `
         <li class="hs-menu-item hs-menu-depth-1" role="none" style="
             content: '';
             width: 40px;
             height: 3px;
-            background-color: var(--primary);">
+            background-color: var(--primary);
+            margin-left: 30px;
+            ">
         </li>
         `
 
-    header.insertAdjacentHTML('beforeend', elementToAdd);
-    footer.insertAdjacentHTML('beforeend', elementToAdd);
+    const footerBar = `
+        <li class="hs-menu-item hs-menu-depth-1" role="none" style="
+            content: '';
+            width: 40px;
+            height: 3px;
+            background-color: var(--primary);
+            ">
+        </li>
+        `
+
+    header.insertAdjacentHTML('beforeend', headerBar);
+    footer.insertAdjacentHTML('beforeend', footerBar);
 };
 
 document.addEventListener('DOMContentLoaded', yellowBar);
