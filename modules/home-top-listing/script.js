db5e8fc51d850740ecf956cb357fcdd4bf315761
K1NGAARON <gerardaaron1997@gmail.com>
2023-04-14 16:18:35 +0200

    fixed kordekor stories module

--- modules/home-top-listing/script.js
+++ modules/home-top-listing/script.js
@@ -4,11 +4,9 @@ $(document).ready(function() {
     $('.blog-listing .filter-list li').click(function() {
       $('.blog-listing .filter-list li').removeClass('active');
       $(this).addClass('active');
-
       var tagCode = $(this).attr('data-tag');
       var postColumn = $('.grid-wrapper .post-article');
       var filterValue = postColumn.filter('.'+tagCode);
-
       if(tagCode == 'all') {
         postColumn.show();
       } else {
@@ -20,11 +18,22 @@ $(document).ready(function() {
     var seen = {};
     $('.blog-listing .filter-list li').each(function() {
       var txt = $(this).text();
-
-      if (seen[txt]) {
+      if (seen[txt])
         $(this).remove();
-      } else {
+      else
         seen[txt] = true;
-      }
+    });
+    
+    $(document).ready(function() {
+        const stories = document.querySelectorAll('.general-wrapper .kordekor-stories');
+        $(stories).remove();
+  
+        const allPosts = document.querySelectorAll('.general-wrapper .post-article ');  
+        const maxItems = 6;
+  
+        const length = allPosts.length;
+        const difference = length - maxItems;
+  
+        $(allPosts).slice(-difference).remove();
     });
   });
