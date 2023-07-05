9aa6b35b92e6a2198516b49ad6fe10c6e04eee4b
K1NGAARON <gerardaaron1997@gmail.com>
2023-07-05 20:12:58 +0200

    fixed scripts

--- modules/mobile-menu/header.js
+++ modules/mobile-menu/header.js
@@ -1,37 +1,45 @@
 function addModalContainer() {
-    const placement = document
-}
-
-function addHamburgerMenu() {
+    const placement = document;
+  }
+  
+  function addHamburgerMenu() {
     const header = document.querySelector('.header .header-menu__menu ul');
     const hamburgerMenu = `
-        <i id="showMenu" class="hamburger-icon fa-regular fa-bars"></i>
+      <img id="showMenu" src="https://26913733.fs1.hubspotusercontent-eu1.net/hubfs/26913733/menu-icon.png" alt="hamburger">
     `;
-
+  
     header.insertAdjacentHTML('beforeend', hamburgerMenu);
-}
-
-function openModal(e) {
+  }
+  
+  function openModal(e) {
     e.preventDefault();
     modalContainer.classList.add('active');
-}
-
-function closeModal() {
+  }
+  
+  function closeModal() {
     modalContainer.classList.remove('active');
-}
-
-let modalContainer; //
-
-document.addEventListener('DOMContentLoaded', function() {
+  }
+  
+  function addClassOnEscape(event) {
+    if (event.key === 'Escape') {
+        closeModal();
+    }
+  }
+  
+  let modalContainer;
+  
+  document.addEventListener('DOMContentLoaded', function () {
     addHamburgerMenu();
-
+  
     modalContainer = document.querySelector('#modal-container');
-
+  
     const showIcon = document.querySelector('#showMenu');
     const showIconSM = document.querySelector('.header-menu__toggle-icon');
     const closeIcon = document.querySelector('#closeMenu');
-
+  
     showIcon.addEventListener('click', openModal);
     showIconSM.addEventListener('click', openModal);
     closeIcon.addEventListener('click', closeModal);
-});
+  
+    document.addEventListener('keydown', addClassOnEscape);
+});  
